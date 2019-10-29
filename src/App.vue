113c3144a53bd2977aec51567f438d5720bd10dd
rivasge_sd2023@CEACCS.NET <rivasge_sd2023@CEACCS.NET>
2019-10-29 16:30:37 +0800

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-app>
+    <!-- <v-app-bar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer>
+        <Header />
+      </v-spacer>
+      <v-btn text href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
+        <span class="mr-2">Latest Release</span>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <HelloWorld />
+    </v-content> -->
+    <div id="app">
+      <Header />
+      <router-view />
+    </div>
+  </v-app>
+</template>
+
+<script>
+//import HelloWorld from "./components/HelloWorld";
+import Header from "./components/layout/Header";
+
+export default {
+  name: "App",
+  components: {
+    Header
+  },
+  data: () => ({
+    //
+  })
+};
+</script>
---
+++ src/views/About.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="about">
+   <b> DEVELOPERS</b><br>
+    <jessa/><br>
+    <ivy/><br>
+    <gen/>
+  </div>
+</template>
+
+<script>
+import jessa from "../components/Jessa";
+import ivy from "../components/Ivy";
+import gen from "../components/Gen";
+
+
+export default {
+  name: "About",
+  components: {
+    jessa,
+    ivy,
+    gen
+  }
+};
+</script>
+<style scoped>
+  .about{}
+</style>
